ff3430f08c690ae87763628891af8660e12df344
BeTeP-STH <pionerka@ghmail.com>
2020-06-06 14:39:37 -0500

    fixed MFA TOC parser

--- mfa_extract.py
+++ mfa_extract.py
@@ -13,62 +13,70 @@ import lzma
 import re
 import configparser
 
-FLINT = 'mstflint'
-XZ_MARKER = b'\xFD7zXZ'
-MTOC_STRUCT = '32s8x150s38x'
-META_STRUCT = '>II16x'
-MTOC_REC_LEN = struct.calcsize(MTOC_STRUCT)
-META_REC_LEN = struct.calcsize(META_STRUCT)
-
-BUFFER = bytearray()
-MTOC = []
+FLINT = "mstflint"
 
 def read_buff(fn, n=-1):
     with open(fn, 'rb') as f:
         return f.read(n)
 
-def parse_mtoc(mtoc):
-    global MTOC
-    for r in struct.iter_unpack(MTOC_STRUCT, mtoc):
-        d = [s for s in 'PSID\0{}{}'.format(*map(bytes.decode,r)).split('\0') if s]
-        MTOC.append(dict(zip(d[::2], d[1::2])))
+def lzma_decompress(buf):
+    decomp = lzma.LZMADecompressor(memlimit=0x10000000)
+    try:
+        return decomp.decompress(buf)
+    except lzma.LZMAError:
+        pass
+    return b''
 
-def parse_meta(meta, psid=''):
-    global MTOC
-    o, s = 0, 0
-    for i, r in enumerate(struct.iter_unpack(META_STRUCT, meta)):
-        MTOC[i].update({'OFFSET': r[0], 'SIZE': r[1]})
-        if MTOC[i]['PSID'] == psid:
-            o, s = r
-    return o, s
-
-def save_bin(fn, lzma_off, bin_off, bin_len):
-    global BUFFER
+def save_bin(fn, buff, bin_off, bin_len):
     decomp = lzma.LZMADecompressor(memlimit=0x10000000)
     with open(fn, 'wb') as f:
         try:
-            f.write(decomp.decompress(BUFFER[lzma_off:])[bin_off:bin_off + bin_len])
+            f.write(decomp.decompress(buff)[bin_off:bin_off + bin_len])
         except lzma.LZMAError:
             pass
     return decomp.eof
 
+def parse_mtoc(buff, compressed, offset, size):
+    mtoc = {}
+    if compressed:
+        off = 0
+        while off < len(buff):
+            a, b, c = struct.unpack_from('>32sB1xH', buff, off)
+            psid = a.decode('ascii').strip('\0')
+            _, pn, _, desc = buff[off+40:off+180].decode('latin1').strip('\0').split('\0')[0:4]
+            mtoc[psid] = { 'pn': pn, 'desc': desc, 'off': [struct.unpack_from('>IHH', buff, 36+off+c+40*i) for i in range(b)]}
+            off += 36 + 40 * b + c
+    return mtoc
+
 def mfa_extract(mfaname, psid):
-    global BUFFER
+    SECTIONS = {}
     BUFFER = bytearray(read_buff(mfaname))
-    mtoc_off = BUFFER.find(XZ_MARKER)
-    meta_off = BUFFER.find(XZ_MARKER, mtoc_off + 1)
-    data_off = BUFFER.find(XZ_MARKER, meta_off + 1)
-    parse_mtoc(lzma.decompress(BUFFER[mtoc_off:]))
-    if psid in [r['PSID'] for r in MTOC]:
-        o, s = parse_meta(lzma.decompress(BUFFER[meta_off:]), psid)
+    if b'MFAR' != BUFFER[0:4]:
+        return 1
+    off = 16
+    for i in range(3):
+        a,b,c,d = struct.unpack_from('>B2xBI4s', BUFFER, off)
+        off += 8
+        SECTIONS[a] = {'offset': off, 'size': c, 'compressed': b and (d == b'\xFD7zX'), 'buff': memoryview(BUFFER[off:off+c]) }
+        if SECTIONS[a]['compressed'] and i < 2:
+            SECTIONS[a]['buff'] = lzma_decompress(SECTIONS[a]['buff'])
+        off += c
+
+    MTOC = parse_mtoc(**SECTIONS[1])
+    if MTOC.get(psid):
         fn = "{}.bin".format(psid)
-        if save_bin(fn, data_off, o, s):
+        for moff in MTOC[psid]['off']:
+            off, size = struct.unpack_from('>ii', SECTIONS[2]['buff'], moff[0])
+            if size > 0:
+                break
+        if save_bin(fn, SECTIONS[3]['buff'], off, size):
             print(subprocess.check_output([FLINT, '-i', fn, 'v']).decode('ascii'))
             print(subprocess.check_output([FLINT, '-i', fn, 'q']).decode('ascii'))
             return 0
+
     else:
-        for r in MTOC:
-            print('{PSID} {PN: <32} {DESCRIPTION:96.96}'.format(**r))
+        for i, psid in enumerate(sorted(MTOC.keys()), 1):
+            print('{i:>3}. {psid:15s}{pn:33s}{desc}'.format(i=i, psid=psid, **MTOC[psid]))
         return 0
     return 1
 
@@ -77,3 +85,4 @@ if __name__ == "__main__":
         print("Usage:\n\t{0} firmware.mfa <PSID>\t - to extract\n\t{0} firmware.mfa l|list\t - to list".format(*sys.argv))
         sys.exit(2)
     sys.exit(mfa_extract(*sys.argv[1:]))
+
